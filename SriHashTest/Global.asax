dbfa03643c7ba685034343b5e56893cf64703b47
Mike Devlin <mike@intrepiware.com>
2023-02-21 14:22:26 -0600

    Add HtmlHelper, default Content-Security-Policy headers

---
+++ SriHashTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SriHashTest.MvcApplication" Language="C#" %>
---
+++ SriHashTest/Services/ScriptsExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SriHashTest.Services
+{
+    public static class ScriptsExtensionMethods
+    {
+        public static IHtmlString RenderSecureScript(this HtmlHelper helper, string url)
+        {
+            var virtualPath = (url.StartsWith("~/")) ? url : $"~/{url}";
+            var absPath = VirtualPathUtility.ToAbsolute(virtualPath);
+
+            var filePath = HttpContext.Current.Server.MapPath(virtualPath);
+            var hash = string.Empty;
+
+            using (var sha384Hash = SHA384.Create())
+            {
+                hash = Convert.ToBase64String(sha384Hash.ComputeHash(File.ReadAllBytes(filePath)));
+            }
+
+            return new HtmlString($"<script src=\"{absPath}\" integrity=\"sha384-{hash}\" crossorigin=\"anonymous\"></script>");
+        }
+    }
+}
