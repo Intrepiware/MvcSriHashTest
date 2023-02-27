cf360b520b22082a62feba1557271b5de9b25b85
Mike Devlin <mike@intrepiware.com>
2023-02-27 05:46:42 -0600

    Add nonce to CSP header to allow inline script

--- SriHashTest/App_Start/FilterConfig.cs
+++ SriHashTest/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using SriHashTest.Services;
+using System.Web;
 using System.Web.Mvc;
 
 namespace SriHashTest
@@ -8,6 +9,7 @@ namespace SriHashTest
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AddCspHeaders());
         }
     }
 }
---
+++ SriHashTest/Services/AddCspHeaders.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace SriHashTest.Services
+{
+    public class AddCspHeaders : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var nonce = Guid.NewGuid().ToString("n");
+            filterContext.Controller.ViewBag.__cspHeaderNonce = nonce;
+
+            filterContext.HttpContext.Response.Headers["Content-Security-Policy"] = $"script-src 'self' 'nonce-{nonce}' https://cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js; style-src 'self'";
+        }
+    }
+}
